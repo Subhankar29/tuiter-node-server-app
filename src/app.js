f4a42e118b56c34f661dab33d6d149b15f60f184
subhankar29 <subhankar29shah@gmail.com>
2022-11-20 22:35:11 -0500

    Assignment 8

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitController from "./controllers/tuits/tuit-controller.js";
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+HelloController(app)
+UserController(app)
+TuitController(app)
+
+app.listen(4000)
